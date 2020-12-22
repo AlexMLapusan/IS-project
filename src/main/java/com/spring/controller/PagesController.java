cc5388b76cb3e32ea28aba98675e5843be3c7417
AlexMLapusan <alex.m.lapusan@gmail.com>
2020-12-22 15:37:43 +0200

    Exemplu cu cum ar putea fi stilizata o caseta de optiuni in pagina de buy (also sterge toate br-urile)

--- src/main/java/com/spring/controller/PagesController.java
+++ src/main/java/com/spring/controller/PagesController.java
@@ -6,20 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PagesController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/home_user")
     public String home(){ return "home_user"; }
+
     @RequestMapping("/login")
     public String login()
     {
         return "login";
     }
+
     @RequestMapping("/register")
     public String register() { return "register"; }
 
     @RequestMapping("/buy")
     public String buy() { return "buy"; }
+
     @RequestMapping("/user_account ")
     public String user_account() {return "user_account";}
+
     @RequestMapping("/home_admin")
     public String homeForAdmin(){ return "home_admin"; }
 
